c5d1535a528818e5b308feb8458f6a76d4f500f2
khaliqtaimoor6-codes <khaliqtaimoor6@gmail.com>
2026-02-26 21:28:35 +0500

    remove: spirit dragon animation component

--- app/(app)/layout.tsx
+++ app/(app)/layout.tsx
@@ -1,12 +1,10 @@
 import Sidebar from "@/components/Sidebar";
-import SpiritDragon from "@/components/SpiritDragon";
 
 export default function DashboardLayout({ children }: { children: React.ReactNode }) {
   return (
     <div className="min-h-screen bg-background transition-colors duration-300">
-      <SpiritDragon />
       <Sidebar />
-      <main className="lg:pl-64 relative z-10">
+      <main className="lg:pl-64">
         <div className="mx-auto max-w-6xl px-4 py-8 sm:px-6 lg:px-8">
           {children}
         </div>
